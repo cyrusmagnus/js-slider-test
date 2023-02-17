202ae34662025bdb0c046e9bcfd4c9d03908f77d
cyrusmagnus <kiryasovnikita0@gmail.com>
2023-02-17 14:40:13 +0300

    update-key-management

--- main.js
+++ main.js
@@ -12,8 +12,17 @@ let activeSlideIndex = 0;
 upBtn.addEventListener('click', () => {
   changeSlide('up');
 })
+
 downBtn.addEventListener('click', () => {
   changeSlide('down');
+})
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowUp') {
+    changeSlide('up');
+  } else if (event.key === 'ArrowDown') {
+    changeSlide('down');
+  }
 })
 
 function changeSlide(direction) {
